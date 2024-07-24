e9ea011788ccdbd2f088ee0f5154f54545c8c8b5
Dario Jularic <dariojularic0@gmail.com>
2024-07-24 20:17:08 +0200

    Create async function to get jokes

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 import './style.css'
+
+const loadJoke = async () => {
+  try {
+    const chuckNorrisFetch = await fetch("https://api.chucknorris.io/jokes/random", {
+      headers: {
+        Accept: "application/json"
+      }
+    })
+    const jokeData = await chuckNorrisFetch.json();
+    document.querySelector("#loadingJoke").innerHTML = jokeData.value;
+    console.log(jokeData.value)
+  }
+  catch(error) {
+    console.log(error)
+  }
+}
+console.log(loadJoke())
+
+document.querySelector("#loadJokeBtn").addEventListener("click", loadJoke);
